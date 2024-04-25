c1dd3c74e5f2953c38116c2e862a64e8afd35023
FabianoLuiz3103 <116929502+FabianoLuiz3103@users.noreply.github.com>
2024-04-25 14:21:05 -0300

    Update Aplicacao.java

--- ABB_KRAFT-HEINZ/src/br/com/fiap/aplicacao/Aplicacao.java
+++ ABB_KRAFT-HEINZ/src/br/com/fiap/aplicacao/Aplicacao.java
@@ -3,11 +3,7 @@ package br.com.fiap.aplicacao;
 // >>>>>>>>>>>>> INTEGRANTES <<<<<<<<<<<<<<<<<<<
 
 /**
- * @author Eduardo Henriques Bertelli --------  RM:92966
  * @author Fabiano Luiz Santos de Sanatana ---- RM:96044
- * @author Fernando Igino Machado ------------- RM:94712
- * @author Gustavo Crosera Lobo --------------- RM:93202
- * @author Lucas Aguido Mazzeto --------------- RM:93303
  */
 
 import java.util.Scanner;
@@ -20,7 +16,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 
-		// USAR ESSES REGISTROS DE MATRÕCULA DOS "FUNCION¡RIOS" PARA FAZER LOGIN E
+		// USAR ESSES REGISTROS DE MATR√çCULA DOS "FUNCION√ÅRIOS" PARA FAZER LOGIN E
 		// SEGUIR COM O PORGRAMA:
 		// 12345A, 23456B, 34567C, 45678D, 56789E, 67890F, 78901G, 89012H, 90123I,
 		// 01234J, 23456K, 34567L, 45678M, 56789N, 67890O, 78901P, 89012Q, 90123R,
@@ -36,7 +32,7 @@ public class Aplicacao {
 
 		do {
 
-			System.out.println("\n\nOl·, deseja fazer login? (S/N)");
+			System.out.println("\n\nOl√°, deseja fazer login? (S/N)");
 			System.out.println("  N - PARA ENCERRAR PROGRAMA!");
 			login = teclado.nextLine().toUpperCase().charAt(0);
 
@@ -52,8 +48,8 @@ public class Aplicacao {
 					System.out.println(funcaoIte.apresentarFuncionario());
 
 					do {
-						System.out.println("\n\t\tINSIRA" + "\n\t<I>: Para menu de inserÁ„o;"
-								+ "\n\t<T>: Para menu de triagem;" + "\n\t<E>: Para menu de exibiÁ„o;"
+						System.out.println("\n\t\tINSIRA" + "\n\t<I>: Para menu de inser√ß√£o;"
+								+ "\n\t<T>: Para menu de triagem;" + "\n\t<E>: Para menu de exibi√ß√£o;"
 								+ "\n\t<X>: Para voltar para menu de login.");
 						opcao = teclado.nextLine().toUpperCase();
 
@@ -70,22 +66,22 @@ public class Aplicacao {
 						case "X":
 							if (!filaAmbiental.isEmpty() || !filaSocial.isEmpty() || !filaGovernanca.isEmpty()) {
 								System.out.println("\n\nERRO AO VOLTARA PARA A TELA DE LOGIN!");
-								System.out.println("Ainda h· mensagens para serem tratadas!");
+								System.out.println("Ainda h√° mensagens para serem tratadas!");
 							} else {
 								System.out.println("\nVOLTANDO PARA LOGIN! ");
 								opcao = "-";
 							}
 							break;
 						default:
-							System.out.println("\nOP«√O INV¡LIDA! ");
+							System.out.println("\nOP√á√ÉO INV√ÅLIDA! ");
 							break;
 						}
 
 					} while (!opcao.equals("-"));
 				} else
-					System.out.println("\t\nFUNCION¡RIO N√O ENCONTRADO / N√O CADASTRADO!");
+					System.out.println("\t\nFUNCION√ÅRIO N√ÉO ENCONTRADO / N√ÉO CADASTRADO!");
 			} else {
-				System.out.println("\n" + "" + "FIM DA EXECU«√O! ");
+				System.out.println("\n" + "" + "FIM DA EXECU√á√ÉO! ");
 			}
 
 		} while (login == 'S');
